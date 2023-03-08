dd041f4438ee790e36c9a6b0675fbda73b593969
Kattebu <butskoev@tut.by>
2023-03-08 19:10:12 +0300

    ex1

--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 47: Задать двумерный массив размером m*n случайными вещественными числами
+double[,] GetMatrix(int m, int n)
+{
+    double[,] matrix = new double[m,n];//[строчка, столбец]
+    for(int i =0; i<matrix.GetLength(0); i++)//строчки rows=matrix.GetLength(0)
+    {
+        for (int j =0; j<matrix.GetLength(1); j++)
+        {
+            matrix[i,j]=new Random().NextDouble();
+        }
+    }
+    return matrix;//вернули заполненную таблицу
+}
+//метод для печати матрицы (двумерного массива)
+void PrintMatrix(double[,] matr)
+{
+for(int i =0; i<matr.GetLength(0); i++)//проход по строчкам
+    {
+        for (int j =0; j<matr.GetLength(1); j++)//проход по столбцам
+        {
+            Console.Write(matr[i,j]+"\t");
+        }
+        Console.WriteLine();
+    }
+}
+double[,] resultMatrix=GetMatrix(5,4);
+PrintMatrix(resultMatrix);
